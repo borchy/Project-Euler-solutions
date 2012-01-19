f9906798abe3f9d8bbf75d4a73d8d4fbca4145df
borchy <falstad@gmail.com>
2012-01-20 00:03:47 +0200

    wadawdwadadwwaddwaawdwdawad

--- dawda_.rb
+++ dawda_.rb
@@ -7,15 +7,7 @@ describe NumberToWord do
     end
     it "3" do
       number_to_word.number_to_word(3).should == "three"
-    
-      number_to_word.number_to_word(101).should == "onehundredandone"
-    end
-    it "112" do
-      number_to_word.number_to_word(112).should == "onehundredandtwelve"
-    end
-    it "142" do
-      number_to_word.number_to_word(142).should == "onehundredandfortytwo"
-    end
+
     it "167" do
       number_to_word.number_to_word(167).should == "onehundredandsixtyseven"
     end
