f27a37ababcc12a1fa095ea2c4cd790fdf39a680
borchy <falstad@gmail.com>
2012-01-20 21:45:58 +0200

    DWA

--- dawda.rb
+++ dawda.rb
@@ -7,15 +7,7 @@ describe NumberToWord do
     end
       number_to_word.number_to_word(167).should == "onehundredandsixtyseven"
     end
-    it "204" do
-      number_to_word.number_to_word(204).should == "twohundredandfour"
-    end
-    it "214" do
-      number_to_word.number_to_word(214).should == "twohundredandfourteen"
-    end
-    it "254" do
-      number_to_word.number_to_word(254).should == "twohundredandfiftyfour"
-    end
+    it "204"
     it "264" do
       number_to_word.number_to_word(264).should == "twohundredandsixtyfour"
     end
